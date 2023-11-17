1c97752d601b4f3fe367e74b79db2626ed9b6401
Katsu <jordanjjana@gmail.com>
2023-11-17 18:31:25 +0000

    i still have no idea what i'm doing

---
+++ con.js
@@ -0,0 +1,12 @@
+var readline = require('readline');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+var Cq = rl.question("Input number");
+var b2d, any = [128, 64, 32, 16, 8, 4, 2, 1];
+function main() {
+    Cq - b2d;
+}
+;
+main();
---
+++ con.ts
@@ -0,0 +1,11 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const Cq = rl.question("Input number")
+let b2d: number,any = [128, 64, 32, 16, 8, 4, 2, 1];
+function main () {
+    Cq - b2d
+};
+main();
